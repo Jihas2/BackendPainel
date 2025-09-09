b30d2cd312cf77393dd9a00fcfa88229af0aa14b
ProjetoMensal <jujughozayel@gmail.com>
2025-09-08 21:08:08 -0300

    banco de dados implementado

---
+++ src/main/java/com/web/dev/painelOnline/PainelOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.web.dev.painelOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainelOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainelOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/dev/painelOnline/PainelOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.dev.painelOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainelOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
